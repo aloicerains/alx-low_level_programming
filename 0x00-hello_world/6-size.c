87a714f05f445d33d08e0d4e8b6d14dbe233063e
aloicerains <aloiceokoth98@gmail.com>
2021-06-17 19:00:08 +0300

    removed var declaration

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,11 @@
 
 int main(void)
 {
-	int intType;
-	float floatType;
-	long int longInt;
-	long long int llInt;
-	char charType;
-
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of long int: %zu byte(s)\n", sizeof(longInt));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(llInt));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of char: %zu byte(s)\n", sizeof(char);
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
 
