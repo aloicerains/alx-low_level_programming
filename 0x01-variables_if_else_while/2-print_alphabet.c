1cf98fa9d2774c7e268140f95fa5d08d7914ed34
aloicerains <aloiceokoth98@gmail.com>
2021-06-18 16:05:23 +0300

    Use putchar to print lowercases

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+	}
+	printf("\n");
+	return (0);
+}
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,4 +1,5 @@
 # 0x01-variables_if_else_while
 0. Programs for generating random numbers
 1. Programs obtains the last digit of integer variable
+2. Program prints lowercase alphabets using putchar
 
