4601ca53daaf87e38cc2371a3a119e780048ddaa
aloicerains <aloiceokoth98@gmail.com>
2021-06-24 22:11:15 +0300

    eliminated space after last

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,31 @@ void fizzBuzz(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			if (i == 100)
+				printf("FizzBuzz");
+			else
+				printf("FizzBuzz ");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			if (i == 100)
+				printf("Fizz");
+			else
+				printf("Fizz ");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			if (i == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
 		}
 		else
 		{
-			printf("%d ", i);
+			if (i == 100)
+				printf("%d", i);
+			else
+				printf("%d ", i);
 		}
 	}
 	printf("\n");
