ec542467a58ae1efd0086ddb1fb3deccdaf28b20
aloicerains <aloiceokoth98@gmail.com>
2021-06-24 17:51:37 +0300

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+ * mul - multiplies two integers
+ * @a: integer argument
+ * @b: integer argument being multiplied
+ *
+ * Description: Function multiplies a*b
+ * Return: i the product of multiplication
+ */
+int mul(int a, int b)
+{
+	int i = a * b;
+
+	return (i);
+}
+
+
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,4 +1,5 @@
 More functions and Nested loops
 0. Check uppercases
 1. Checks digits between 0-9. Receives character argument, converts to integer and checks
+2. Function multiples two integers
 
