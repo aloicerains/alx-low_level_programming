6eafa9893cc660ffd254c9336a37886de250d124
aloicerains <aloiceokoth98@gmail.com>
2021-06-24 17:36:55 +0300

    checks digits btw 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,31 @@
+/**
+ * _isdigit - checks whether argument is digit
+ * @c: character argument being checked
+ *
+ * Description: Function takes a character converts to integer
+ * and check for limits between 0-9
+ * Return: 1 if within limits and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int i;
+	int ret;
+	int k = c - '0';
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == k)
+		{
+			ret = 1;
+			break;
+		}
+		else
+		{
+			ret = 0;
+		}
+	}
+	return (ret);
+}
+
+
+
