593a2646d4496e5bd814e3674d85b014721fc80e
aloicerains <aloiceokoth98@gmail.com>
2021-06-25 11:43:25 +0300

    updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,10 @@
 /**
  * more_numbers - prints numbers 0-14
- *@j: integer argument
  *
  * Description: Function prints x14
  * Return: void
  */
 #include "holberton.h"
-void put(int j);
 void more_numbers(void)
 {
 	int i;
@@ -16,30 +14,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j <= 9)
-			{
-				put(j);
-			}
-			else
+			if (j > 9)
 			{
 				_putchar((j / 10) + 48);
-				put(j);
 			}
+				_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
 }
-/**
- * put - prints integer character
- * @j: integer argument
- *
- * Description: prints modulus of character
- * Return: void
- */
-void put(int j)
-{
-	_putchar((j % 10) + 48);
-}
-
-
-
