4361d1d1a4deb91976901df6ff5e730cb2d0f67c
aloicerains <aloiceokoth98@gmail.com>
2021-06-28 20:53:39 +0300

    adjustment

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,15 +1,16 @@
 /**
  * print_array - Function prints array of numbers
- * @x: integer argument
+ * @n : integer size of array
+ * @a : string pointer
  *
  * Description: Function prints elements of an array in straight line
  * Return: void
  */
 #include "holberton.h"
 #include "_putchar.c"
-void printing(int x);
 void print_array(int *a, int n)
 {
+	void printing(int x);
 	int i;
 
 	for (i = 0; i < n; i++)
