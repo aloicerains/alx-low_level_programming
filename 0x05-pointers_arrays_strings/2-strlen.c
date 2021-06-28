8e50e27702246b9c2ad273a60ea6b3fbf8329b53
aloicerains <aloiceokoth98@gmail.com>
2021-06-28 12:55:07 +0300

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - Function determines string length
+ * @s: character argument
+ *
+ * Description: Function computes the overall length of string
+ * Return: length of a string
+ */
+#include "holberton.h"
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+	for (n = 0; *s != '\0'; s++)
+	{
+		n++;
+	}
+	return (n);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,4 +1,5 @@
 Pointers, Arrays, and strings
 0. Reset a an integer to zero
 1. Swapping two variable values
+2. Get the length of a string
 
