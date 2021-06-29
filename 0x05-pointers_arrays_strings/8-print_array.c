34a898fe16eb0d348d96994bd674d501d16f9c57
aloicerains <aloiceokoth98@gmail.com>
2021-06-29 16:08:43 +0300

    used printf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,6 +7,7 @@
  * Return: void
  */
 #include "holberton.h"
+#include <stdio.h>
 #include "_putchar.c"
 void print_array(int *a, int n)
 {
@@ -15,29 +16,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printing(a[i]);
+		printf("%d", a[i]);
 		if (i == n - 1)
 			continue;
-		_putchar(',');
-		_putchar(' ');
+		printf(", ");
 	}
-	_putchar('\n');
-}
-/**
- * printing - prints the integer whether positive or negative
- *@x: integer argument
- *
- * Description: prints both positive and negative numbers
- * Return: void
- */
-void printing(int x)
-{
-	if (x < 0)
-	{
-		_putchar('-');
-		x = -x;
-	}
-	if (x / 10)
-		printing(x / 10);
-	_putchar(x % 10 + '0');
+	printf("\n");
 }
