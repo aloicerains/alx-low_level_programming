8196f23c65bb7cb0e131daa8548098bea923ef43
aloicerains <aloiceokoth98@gmail.com>
2021-07-05 13:10:55 +0300

    copy memory areas

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+/**
+ * _memcpy - prints copies memory bytes
+ * @dest: pointer to the destination
+ * @src: pointer to source
+ * @n: Memory bytes to be copied
+ *
+ * Description: Function copies n bytes from src to memory dest
+ * 
+ * Return: character pointer dest
+ */
+#include "holberton.h"
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *sr = (char *)src;
+	char *des = ( char *)dest; 
+	if ((*des == '\0') && (*sr == '\0'))
+		return ('\0');
+	while (n)
+	{
+		*(des++) = *(sr++);
+		--n; 
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,4 +1,5 @@
 ###0x07_pointers_ arrays_strings
 The directory contains further projects on arrays and pointers. The following are some of the projects:
 * 0-memset.c: Function fills memory with the constant byte
+* 1-memcpy.c: Functions copies n bytes from memory area src to destination dest
 
