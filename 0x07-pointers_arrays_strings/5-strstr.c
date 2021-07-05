e8920285647e15d7216f50786a6da5090cc9f0c9
aloicerains <aloiceokoth98@gmail.com>
2021-07-05 18:30:23 +0300

    added strstr function

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+/**
+ * _strstr- Function finds first occurence of a substring
+ * * @haystack: string pointer to search from
+ * @needle: string pointer being searched
+ *
+ * Return: character pointer
+ */
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+char *_strstr(char *haystack, char *needle)
+{
+	const char *a;
+	const char *b;
+
+	b = needle;
+	if (*b == 0)
+	{
+		return ((char *)haystack);
+	}
+	for (; *haystack != 0; haystack += 1)
+	{
+		if (*haystack != *b)
+			continue;
+		a = haystack;
+		while (1)
+		{
+			if (*b == 0)
+				return ((char *)haystack);
+			if (*a++ != *b++)
+				break;
+		}
+		b = needle;
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -5,4 +5,5 @@ The directory contains further projects on arrays and pointers. The following ar
 * 2-strchr.c: Function locates a character and return new string from the character location
 * 3-strspn.c: Function gets the length of the a prefix substring
 * 4-strpbrk.c: Function searches for a string for any of a set of bytes
+* 5-strstr.c: Function locates substring and return pointer
 
