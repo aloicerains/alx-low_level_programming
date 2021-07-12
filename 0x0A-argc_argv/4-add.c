b09d496ec9a49e516c94a470d6cdbcbd752384c0
aloicerains <aloiceokoth98@gmail.com>
2021-07-12 15:18:19 +0300

    infinite addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+/**
+ * main - program entry point
+ * @argc: argument counter
+ * @argv: pointer to string array
+ *
+ * Description: Function prints the result of adding numerous integers
+ * Return: 0 sucess, 1 failure
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int result, i;
+
+	result = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] != '0')
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -4,4 +4,5 @@ The directory contains projects on passing arguments to main, argc and argv.
 * 1-args.c: Function prints the number of arguments passed
 * 2-args.c: Function prints all the arguments passed
 * 3-mul.c: Program returns the result of multiplying two numbers
+* 4-add.c: Program add and prints the result of additions
 
