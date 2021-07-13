b81dfec26e83077fcf763c64b698691669ec676f
aloicerains <aloiceokoth98@gmail.com>
2021-07-13 13:12:42 +0300

    dynamic array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+/**
+ * create_array - creates and initializes array at runtime
+ * @size: size of array to be created
+ * @c: Character to be initialized
+ *
+ * Return: pointer to the array
+ */
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(*array) * size);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,4 @@
+#### Directory 0x0B_malloc_free
+The direcotry contains the following programs:
+* 0-create_arrays.c: Dynamically creates an array
+
