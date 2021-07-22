a4d21284980b95be3774b4ba29b67cd2c6348fc4
aloicerains <aloiceokoth98@gmail.com>
2021-07-22 16:33:50 +0300

    print number arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+/**
+ * print_numbers - function prints number arguments
+ * @separator: pointer to number separator
+ * @n: number of arguments
+ *
+ *Reeturn: nothing
+ */
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int num;
+
+	va_start(ap, n);
+	for (i = 0; i < n; ++i)
+	{
+		num = va_arg(ap, int);
+		if (separator != NULL && i != n - 1)
+			printf("%d%s ", num, separator);
+		if (separator == NULL && i != n - 1)
+			printf("%d ", num);
+		if (i == (n - 1))
+			printf("%d", num);
+	}
+	va_end(ap);
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -2,4 +2,5 @@
 This directory contains projects on variadic functions.
 The following programs are contained:
 * 0-sum_them_all.c: Functions that returns sum of all it's parameters
+* 1-print_numbers.c: prints out the number arguments
 
