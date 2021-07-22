cf2389bc9bacef93834cbf1db004c8cd3c3290b9
aloicerains <aloiceokoth98@gmail.com>
2021-07-22 21:42:52 +0300

    eiminated extra space

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,11 +22,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", string);
 			if (separator != NULL && i != n - 1)
-				printf("%s ", separator);
+				printf("%s", separator);
 		}
 		if (string == NULL)
 		{
 			printf("(nil)");
+			if (separator != NULL && i != n - 1)
+				printf("%s", separator);
 		}
 
 	}
