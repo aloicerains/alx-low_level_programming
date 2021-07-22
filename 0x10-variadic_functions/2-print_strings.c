0b71069be0aa457e8d06bac97dff8b25f48946de
aloicerains <aloiceokoth98@gmail.com>
2021-07-22 21:36:25 +0300

    eliminated second if

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,8 +27,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (string == NULL)
 		{
 			printf("(nil)");
-			if (separator != NULL && i != n - 1)
-				printf("%s ", separator);
 		}
 
 	}
