4187ced97e4f7f1d7e4806011ae2f791296a1e42
aloicerains <aloiceokoth98@gmail.com>
2021-08-02 15:15:56 +0300

    free2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+/**
+ * free_listint2 - function frees list created
+ * @head: pointer to head argument
+ *
+ * Return: nothing
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while ((*head) != NULL)
+	{
+		tmp = (*head);
+		(*head) = (*head)->next;
+		free(tmp);
+	}
+	(*head) = NULL;
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -5,4 +5,5 @@ The following programs are in this directory:
 * 2-add_nodeint.c: Program adds node a the beginning of the list
 * 3-add_nodeint_end.c: Program adds node at the end of the list_t and terminates the list
 * 4-free_listint.c: Program frees list created before
+* 5-free_listint2.c: Program frees list given a **ptr head argument
 
