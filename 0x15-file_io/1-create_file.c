9df8349c418bd49d4ada9b2c80f18028d20341ff
aloicerains <aloiceokoth98@gmail.com>
2021-08-10 11:51:38 +0300

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@
 #include "main.h"
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd, i, w;
 
 	if (text_content != NULL)
 	{
@@ -27,11 +27,14 @@ int create_file(const char *filename, char *text_content)
 		{
 			if (text_content == NULL)
 			{
-				write(fd, " ", 1);
+				close(fd);
+				return (1);
 			}
-			write(fd, text_content, i);
-			return (1);
+			w = write(fd, text_content, i);
+			if (w != -1)
+				return (1);
 		}
+		close(fd);
 	}
 	return (-1);
 }
