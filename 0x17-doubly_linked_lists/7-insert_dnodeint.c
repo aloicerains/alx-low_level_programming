1c71d0429cf9a1881307eb03a06ee87d1b7d3a9b
aloicerains <aloiceokoth98@gmail.com>
2021-09-17 16:16:58 +0300

    inserts node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,71 +1,48 @@
 /**
- * insert_dnodeint_at_index - Function inserts node a given index
- * @h: head pointer
- * @idx: index of the list where node is to be added
- * @n: data element being added
+ * insert_dnodeint_at_index - Function inserts a new node at a given position
+ * @h: pointer to head
+ * @idx: index location
+ * @n: data element
  *
- * Return: address of the new node or null if failed
+ * Return: address of new node or null if failed
  */
 #include "lists.h"
 #include "1-dlistint_len.c"
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int node_c = 0;
-	unsigned int list_len;
+	unsigned int count = 0;
+	dlistint_t *temp = (*h);
+	unsigned int len;
 	dlistint_t *new_node = NULL;
-	dlistint_t **temp = h;
-	dlistint_t *temp1 = (*h);
 
-	if ((*h) == NULL && idx == node_c)
-	{
-		new_node = add_dnodeint(h, n);
-		return (new_node);
-	}
-	list_len = dlistint_len(temp1);
-	while ((*temp) != NULL)
+	len = dlistint_len(temp);
+	if (temp == NULL && idx == 0)
+		return (add_dnodeint(h, n));
+	else if (temp == NULL && idx == len)
+		return (add_dnodeint_end(h, n));
+	while (temp != NULL)
 	{
 		if (idx == 0)
-			return (add_dnodeint(temp, n));
-		if (idx == list_len)
-			return (add_dnodeint_end(temp, n));
-		if (idx == node_c)
+			return (add_dnodeint(&temp, n));
+		else if (idx == len)
+			return (add_dnodeint_end(&temp, n));
+		if (idx == count)
 		{
-			return (input_index(h, (*temp), n));
+			new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+			if (new_node == NULL)
+				return (NULL);
+			new_node->n = n;
+			new_node->prev = temp->prev;
+			temp->prev = new_node;
+			new_node->next = temp;
+			if (new_node->prev != NULL)
+				new_node->prev->next = new_node;
+			else
+				temp = new_node;
+			return (new_node);
 		}
-		node_c++;
-		(*temp) = (*temp)->next;
+		count++;
+		temp = temp->next;
 	}
 	return (NULL);
 }
-/**
- * input_index - function insert node at index
- * @head: head pointer
- * @next: next pointer
- * @n: data element
- *
- * Return: address
- */
-dlistint_t *input_index(dlistint_t **head, dlistint_t *next, int n)
-{
-	dlistint_t *new_node = NULL;
-
-	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
-	if (new_node != NULL)
-	{
-		new_node->n = n;
-		new_node->prev = next->prev;
-		new_node->next = next;
-		next->prev = new_node;
-		if (new_node->prev != NULL)
-		{
-			new_node->prev->next = new_node;
-		}
-		else
-		{
-			(*head) = new_node;
-		}
-		return (new_node);
-	}
-	return (NULL);
-}
-
